949e45766305719d21c1ab29827b5c9c51227f0c
AhmadLobany <ahmad.lobany@campus.technion.ac.il>
2020-07-01 18:31:38 +0300

    commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Sequelize = require('sequelize')
 //********* Here you should change the password "35533553" => YOUR_OWN_DB_PASSWORD */
-const sequelize = new Sequelize('mysql://root:1234@localhost/sunday_finalProject')
+const sequelize = new Sequelize('mysql://root:35533553@localhost/sunday_finalProject')
 
 const crypto = require('crypto');
 // const { tasks } = require('../../src/stores/mainStore');
